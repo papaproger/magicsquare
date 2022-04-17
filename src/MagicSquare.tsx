86145752b055ba1f9c64b9fe95b6fcb81d4be5dc
papaproger <myunwanted@mail.ru>
2022-04-17 07:09:48 +0300

    -4 extra funcs

--- src/MagicSquare.tsx
+++ src/MagicSquare.tsx
@@ -12,38 +12,25 @@ type PropsType = {
 
 const MagicSquare = React.memo((props: PropsType) => {
 
-    function upButtonClickHandler() {
-        props.preMoveHandler('UP')
-    }
-    function leftButtonClickHandler() {
-        props.preMoveHandler('LEFT')
-    }
-    function rightButtonClickHandler() {
-        props.preMoveHandler('RIGHT')
-    }
-    function downButtonClickHandler() {
-        props.preMoveHandler('DOWN')
-    }
-
     return (
         <div className="magicsquare" style={{left:props.y*120+11, top:props.x*120+11}}>
             <div className="square1"></div>
             <div className="square2">
-                <button onClick={upButtonClickHandler}>UP</button>
+                <button onClick={() => props.preMoveHandler('UP')}>UP</button>
             </div>
             <div className="square3"></div>
             <div className="square4">
-                <button onClick={leftButtonClickHandler}>L</button>
+                <button onClick={() => props.preMoveHandler('LEFT')}>L</button>
             </div>
             <div className="square5">
                 <table><tr><td>{props.value}</td></tr></table>
             </div>
             <div className="square6">
-                <button onClick={rightButtonClickHandler}>R</button>
+                <button onClick={() => props.preMoveHandler('RIGHT')}>R</button>
             </div>
             <div className="square7"></div>
             <div className="square8">
-                <button onClick={downButtonClickHandler}>DN</button>
+                <button onClick={() => props.preMoveHandler('DOWN')}>DN</button>
             </div>
             <div className="square9"></div>
         </div>
